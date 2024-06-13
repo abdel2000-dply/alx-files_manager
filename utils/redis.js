143b6c9def391191c49f6ae8afd5e2267fa6041c
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-13 13:24:19 +0100

    Add Redis client utility with get, set, and del methods

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+import { error } from 'console';
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.error(error);
+    });
+
+    this.getAsync = promisify(this.client.get).bind(this.client)
+    this.setAsync = promisify(this.client.set).bind(this.client)
+    this.delAsync = promisify(this.client.del).bind(this.client)
+  }
+
+  isAlive() {
+    return this.client.connected
+  }
+
+  async get(key) {
+    return this.getAsync(key)
+  }
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration)
+  }
+  async del(key) {
+    await this.delAsync(key)
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;

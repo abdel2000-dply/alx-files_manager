e05f5ae0f714e3cf53398a9888fd1a9ce39eedc2
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-14 18:05:26 +0100

    Add server.js file with Express setup and route configuration

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes/index';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

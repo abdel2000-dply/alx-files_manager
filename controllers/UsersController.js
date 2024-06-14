38ce0a5d0b315ea55bd3b48317f38a22bfd61dc0
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-14 19:31:40 +0100

    chore: Update UsersController to use async/await for database operations

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,23 +2,23 @@ import dbClient from "../utils/db";
 import sha1 from "sha1";
 
 class UsersController {
-  static postNew(req, res) {
+  static async postNew(req, res) {
     const { email, password } = req.body;
 
     if (!email) return res.status(400).send({ error: 'Missing email' });
     if (!password) return res.status(400).send({error: 'Missing password'});
 
-    const user = dbClient.db.users.findOne({ email });
+    const user = await dbClient.db.collection('users').findOne({ email });
     if (user) return res.status(400).send({ error: 'Already exist' });
   
     const hashedPassword = sha1(password);
   
-    const newUser = dbClient.db.users.insertOne({ email: email, password: hashedPassword });
+    const newUser = await dbClient.db.collection('users').insertOne({ email: email, password: hashedPassword });
     return res.status(201).send({ id: newUser.insertedId, email: email });
   }
 
-  static getMe(req, res) {
-    const user = dbClient.db.users.findOne({ email: req.user.email });
+  static async getMe(req, res) {
+    const user = await dbClient.db.collection('users').findOne({ email: req.user.email });
     return res.status(200).send({ id: user._id, email: user.email });
   }
 }

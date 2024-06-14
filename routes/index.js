0fb6296f0734fb063f8959d0a1453d7bfe555b58
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-14 18:42:06 +0100

    Add users routes and controller for creating new users

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+
 
 const router = express.Router();
 
+// status and stats routes
 router.get('/status', AppController.getStatus);
-
 router.get('/stats', AppController.getStats);
+
+// users routes
+router.post('/users', UsersController.postNew);
 
 export default router;

5ab1c189177fb1415e8ecf576bfe4ff66a856340
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-14 18:41:48 +0100

    Add UsersController with postNew and getMe methods

---
+++ controllers/UsersController.js
@@ -0,0 +1,26 @@
+import dbClient from "../utils/db";
+import sha1 from "sha1";
+
+class UsersController {
+  static postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({error: 'Missing password'});
+
+    const user = dbClient.db.users.findOne({ email });
+    if (user) return res.status(400).send({ error: 'Already exist' });
+  
+    const hashedPassword = sha1(password);
+  
+    const newUser = dbClient.db.users.insertOne({ email: email, password: hashedPassword });
+    return res.status(201).send({ id: newUser.insertedId, email: email });
+  }
+
+  static getMe(req, res) {
+    const user = dbClient.db.users.findOne({ email: req.user.email });
+    return res.status(200).send({ id: user._id, email: user.email });
+  }
+}
+
+export default UsersController;

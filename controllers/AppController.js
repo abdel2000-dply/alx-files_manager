d7657766f8422385bdfd5910bd9af10d8beae785
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-14 18:05:11 +0100

    Add AppController with getStatus and getStats methods

---
+++ controllers/AppController.js
@@ -0,0 +1,25 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+
+class AppController {
+  static getStatus(req, res) {
+    const redis = redisClient.isAlive();
+    const db = dbClient.isAlive();
+    res.json({ redis: redis, db: db });
+  }
+
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then((nbusers, nbfiles) => {
+        res.json({ users: nbusers, files: nbfiles });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send(error.message);
+      });
+  }
+
+}
+
+export default AppController;

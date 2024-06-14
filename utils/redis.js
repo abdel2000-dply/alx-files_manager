691317c5ca19627504c111f1f2d296431b62002e
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-14 14:09:13 +0100

    Update Redis client utility to handle connection status

--- utils/redis.js
+++ utils/redis.js
@@ -1,21 +1,26 @@
-import { error } from 'console';
 import redis from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
+    this.isConnected = true;
     this.client.on('error', (error) => {
       console.error(error);
+      this.isConnected = false;
     });
 
     this.getAsync = promisify(this.client.get).bind(this.client)
     this.setAsync = promisify(this.client.set).bind(this.client)
     this.delAsync = promisify(this.client.del).bind(this.client)
+  
+    this.client.on('connect', () => {
+      this.isConnected = true;
+    });
   }
 
   isAlive() {
-    return this.client.connected
+    return this.isConnected;
   }
 
   async get(key) {

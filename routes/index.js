e3a6cc3769ce29db2e5b8604b6c7c87d7f51685e
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-15 01:28:34 +0100

    Add authentication routes and controller for user authentication

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthContoller from '../controllers/AuthController';
 
 
 const router = express.Router();
@@ -11,5 +12,10 @@ router.get('/stats', AppController.getStats);
 
 // users routes
 router.post('/users', UsersController.postNew);
+
+// Authentication routes
+router.get('/connect', AuthContoller.getConnect);
+router.get('/disconnect', AuthContoller.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 export default router;

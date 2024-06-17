671127513c11244008c8d8bd5c75ef1209ec4f6e
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-17 22:19:15 +0100

    Add routes for getting file details and listing files

--- routes/index.js
+++ routes/index.js
@@ -20,5 +20,7 @@ router.get('/users/me', UsersController.getMe);
 
 // file routes
 router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
 export default router;

38e8a811faacbe2e02b6b2f40bb838c40f0ec3f6
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-19 01:00:46 +0100

    main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+import dbClient from './utils/db';
+
+const waitConnection = () => {
+    return new Promise((resolve, reject) => {
+        let i = 0;
+        const repeatFct = async () => {
+            await setTimeout(() => {
+                i += 1;
+                if (i >= 10) {
+                    reject(new Error('Connection timeout'));
+                }
+                else if(!dbClient.isAlive()) {
+                    repeatFct();
+                }
+                else {
+                    resolve();
+                }
+            }, 1000);
+        };
+        repeatFct();
+    });
+};
+
+(async () => {
+    console.log(dbClient.isAlive());
+    try {
+        await waitConnection();
+        console.log(dbClient.isAlive());
+        console.log(await dbClient.nbUsers());
+        console.log(await dbClient.nbFiles());
+    } catch (error) {
+        console.error('Failed to wait for connection:', error);
+    }
+})();
